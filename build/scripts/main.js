9f32631efe9e12330eb79350b9892086aaec39d4
Kirill <kfedorov78@gmail.com>
2017-07-16 23:39:33 +0300

    player

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,3 +1,27 @@
-// $(document).ready(function(){
-// 	$(".hb a").animated("zoomInUp");
-// });
+$(document).ready(function(){
+	function wow(){
+		new WOW().init();
+	};
+
+	function scroll(){
+		$('a[href^="#"]').bind("click", function(e){
+		  var anchor = $(this);
+		  $('html, body').stop().animate({
+		   scrollTop: $(anchor.attr("href")).offset().top
+		  }, 1000);
+		  e.preventDefault();
+		 });
+		 return false;﻿
+	}
+
+	function player(){
+		$('#play').click(function(){
+			$('.mv-wrapper').addClass('fade');
+			$("iframe[src]").attr("src", "https://www.youtube.com/embed/a1Y73sPHKxw?rel=0&amp;autoplay=1");
+		});
+	};
+	
+	wow();
+	scroll();
+	player();
+});
